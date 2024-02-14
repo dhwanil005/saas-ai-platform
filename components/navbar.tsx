1d7e12cffc9764e69648e529373aa5b6adf6ae38
dhwanil005 <dhwanil.shah00@gmail.com>
2024-02-14 00:06:41 -0500

    Mobile sidebar implemented, fix hydration errors in the end using Suspense or dynamic rendering whichever suits the case

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,13 +2,12 @@ import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import {Menu} from "lucide-react";
 import Sidebar from "@/components/sidebar";
+import MobileSidebar from "./mobile-sidebar";
 
 const Navbar = () => {
     return (
-        <div>
-            <Button variant="ghost" size="icon" className="md:hidden">
-                <Menu/>
-            </Button>
+        <div className="flex items-center p-4">
+            <MobileSidebar/>
             <div className="flex w-full justify-end">
                 <UserButton signInUrl="/"/>
             </div>
