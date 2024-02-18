cfdcf542918ddc2990769603659efaf19f5f2586
dhwanil005 <dhwanil.shah00@gmail.com>
2024-02-17 22:47:51 -0500

    Customer support set up using crisp

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import "./globals.css";
 import { ModalProvider } from "@/components/modal-provider";
 import { ToasterProvider } from "@/components/toaster-provider";
+import { CrispProvider } from "@/components/crisp-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
         <body className={inter.className}>
           <ModalProvider/>
           <ToasterProvider/>
+          <CrispProvider/>
           {children}
           </body>
       </html>
---
+++ components/crisp-chat.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { useEffect } from "react";
+import {Crisp} from "crisp-sdk-web";
+
+export const CrispChat = () => {
+    useEffect(()=> {
+        Crisp.configure("d85eeb01-5005-4adc-9027-9cf4d0642e05");
+    },[]);
+
+    return null;
+}
